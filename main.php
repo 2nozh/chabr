1914b68ff6f0ba32aa8778a4b303b61ec9b48b50
2nozha <Anna-Seslavinskaya@mail.ru>
2020-04-10 22:18:30 +0300

    bd connected!

--- main.php
+++ main.php
@@ -8,18 +8,28 @@
 <body>
 
 <?php
-
 $name=$_POST["name"];
 $password=$_POST["password"];
 echo "<h1 class='title'>Welcome," . $name . "</h1>";
-/*$serverName="localhost";
-$userName="us";
-$pwd="root";
-$conn=new mysqli($serverName,$userName,$pwd);
+$serverName="localhost";
+$userName="j90093e2";
+$pwd="Baso4ka";
+try {
+    $conn=new PDO('mysql:host=localhost;dbname=scheme','us','root');
+}catch (PDOException $e){
+    print "Error!!!!" . $e->getMessage() . "<br/>";
+    die();
+};
+
 if($conn){
     echo "connected";
+    $stmt = $conn->query('SELECT * FROM Sotrudnik');
+    while ($row = $stmt->fetch())
+    {
+        echo $row['name'] . "\n";
+    }
 }else echo "failed - ";
-die(print_r(sqlsrv_errors(),true));*/
+die();
 ?>
 
 
